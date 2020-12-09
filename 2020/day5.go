30cd4fdd9aaee1b5dc3a525c03fb3252f33aa639
Paul Armstrong <psa@users.noreply.github.com>
2020-12-08 18:07:03 -0800

    2020 day 5 exercise 1

---
+++ 2020/day5.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+/* Errors should be printed to STDERR not STDOUT */
+func die(err error) {
+	fmt.Fprintf(os.Stderr, "%s\n", err)
+	os.Exit(1)
+}
+
+func readFile(filename string) ([]string, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(data), "\n")
+	/* Split on an empty returns an array of length 1 */
+	if len(data) == 0 || string(data) == "\n" || len(lines) == 0 {
+		return nil, errors.New("Empty file")
+	}
+
+	return lines, nil
+}
+
+func findSeatRow(seat string) int {
+	var seatNumber []byte
+	if len(seat) != 7 {
+		fmt.Fprintf(os.Stderr, "Bad seat ID: %s, length %d instead of 7\n", seat, len(seat))
+		return 0
+	}
+	for _, c := range seat {
+		switch c {
+		case 'F':
+			seatNumber = append(seatNumber, '0')
+		case 'B':
+			seatNumber = append(seatNumber, '1')
+		default:
+			fmt.Fprintf(os.Stderr, "Error, bad position identifier in %s: %c\n", seat, c)
+			return 0
+		}
+	}
+	if row, err := strconv.ParseUint(string(seatNumber), 2, 8); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return 0
+	} else {
+		return int(row)
+	}
+}
+
+func findSeatColumn(seat string) int {
+	var seatNumber []byte
+	if len(seat) != 3 {
+		fmt.Printf("Bad seat ID: %s, length %d instead of 3\n", seat, len(seat))
+		return 0
+	}
+	for _, c := range seat {
+		switch c {
+		case 'L':
+			seatNumber = append(seatNumber, '0')
+		case 'R':
+			seatNumber = append(seatNumber, '1')
+		default:
+			fmt.Printf("Error, bad position identifier in %s: %c\n", seat, c)
+			return 0
+		}
+	}
+	if row, err := strconv.ParseUint(string(seatNumber), 2, 4); err != nil {
+		fmt.Println(err)
+		return 0
+	} else {
+		return int(row)
+	}
+}
+
+func generateSeatID(row int, column int) int {
+	if row < 0 || row > 127 {
+		fmt.Println("Row is out of bounds", row)
+		return 0
+	}
+	if column < 0 || column > 7 {
+		fmt.Println("Column is out of bounds", column)
+		return 0
+	}
+	return (row * 8) + column
+}
+
+func generateSeatIDs(inputData []string) []int {
+	var seatIDs []int
+
+	for _, line := range inputData {
+		if len(line) == 0 {
+			continue
+		}
+		if len(line) != 10 {
+			fmt.Println("Error, short line:", line)
+			continue
+		}
+		row := line[:7]
+		column := line[7:]
+		seatIDs = append(seatIDs, generateSeatID(findSeatRow(row), findSeatColumn(column)))
+	}
+	return seatIDs
+}
+
+func findHighestSeatID(seatIDs []int) (int, error) {
+	if len(seatIDs) < 1 {
+		return 0, errors.New("Not passed any seat IDs")
+	}
+	sort.Ints(seatIDs)
+	highest := seatIDs[len(seatIDs)-1:]
+	return highest[0], nil
+}
+
+func main() {
+	var fileName string
+	var part2 bool
+
+	flag.StringVar(&fileName, "f", "input/5", "Input file")
+	flag.BoolVar(&part2, "2", false, "Compute part 2 of the exercise")
+	flag.Parse()
+
+	inputData, err := readFile(fileName)
+	if err != nil {
+		die(err)
+	}
+
+	seatIDs := generateSeatIDs(inputData)
+
+	result, err := findHighestSeatID(seatIDs)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+	}
+	fmt.Println(result)
+}
---
+++ 2020/day5_test.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func createTestFile(contents []byte, t *testing.T) string {
+	tempFile, err := ioutil.TempFile("/tmp", "unit-test.*")
+	if err != nil {
+		t.Log("Failed to create temp file", err)
+		t.Fail()
+	}
+
+	if len(contents) != 0 {
+		if _, err := tempFile.Write(contents); err != nil {
+			t.Log("Failed to write to file:", err)
+			t.Fail()
+		}
+	}
+
+	if err := tempFile.Close(); err != nil {
+		t.Log("Failed to write to file:", err)
+		t.Fail()
+	}
+
+	return tempFile.Name()
+}
+
+func TestCreateTestFile(t *testing.T) {
+	contents := []byte("Line1\nLine2")
+	fileName := createTestFile(contents, t)
+	defer os.Remove(fileName)
+
+	readContents, err := readFile(fileName)
+	if err != nil {
+		t.Log("Failed to read file:", err)
+		t.Fail()
+	}
+
+	if !reflect.DeepEqual(strings.Split(string(contents), "\n"), readContents) {
+		t.Log("Error, contents of test file differs", readContents)
+		t.Fail()
+	}
+}
+
+func TestReadFile(t *testing.T) {
+	contents := []byte("Line1\nLine2")
+	fileName := createTestFile(contents, t)
+	defer os.Remove(fileName)
+
+	readContents, err := readFile(fileName)
+	if err != nil {
+		t.Log("Failed to read file:", err)
+		t.Fail()
+	}
+
+	if !reflect.DeepEqual(strings.Split(string(contents), "\n"), readContents) {
+		t.Log("Error, contents of test file differs", readContents)
+		t.Fail()
+	}
+}
+
+func TestReadFileSingleLine(t *testing.T) {
+	contents := []byte("Line1")
+	fileName := createTestFile(contents, t)
+	defer os.Remove(fileName)
+
+	readContents, err := readFile(fileName)
+	if err != nil {
+		t.Log("Failed to read file:", err)
+		t.Fail()
+	}
+
+	if !reflect.DeepEqual(strings.Split(string(contents), "\n"), readContents) {
+		t.Log("Error, contents of test file differs", readContents)
+		t.Fail()
+	}
+}
+
+func TestReadFileNonexistent(t *testing.T) {
+	_, err := readFile("unit-test.does-not-exist")
+	if err == nil {
+		t.Log("Failed to error on non-existent file")
+		t.Fail()
+	}
+}
+
+func TestReadFileEmpty(t *testing.T) {
+	tempFile, err := ioutil.TempFile("/tmp", "unit-test.*")
+	if err != nil {
+		t.Log("Failed to create temp file", err)
+		t.Fail()
+	}
+	defer os.Remove(tempFile.Name())
+
+	if err := tempFile.Close(); err != nil {
+		t.Log("Failed to write to file:", err)
+		t.Fail()
+	}
+
+	readContents, err := readFile(tempFile.Name())
+	if err == nil {
+		t.Log("Should error on empty file, got:", readContents)
+		t.Fail()
+	}
+}
+
+func TestReadFileSingleNewline(t *testing.T) {
+	contents := []byte("\n")
+	fileName := createTestFile(contents, t)
+	defer os.Remove(fileName)
+
+	readContents, err := readFile(fileName)
+	if err == nil {
+		t.Log("Got contents where there should be none:", readContents)
+		t.Fail()
+	}
+}
+
+func TestFindSeatRow(t *testing.T) {
+	seat := "FBFBBFF"
+	row := findSeatRow(seat)
+	if row != 44 {
+		t.Log("Expected row 44, got", row)
+		t.Fail()
+	}
+
+	seat = "FBFBBF"
+	row = findSeatRow(seat)
+	if row != 0 {
+		t.Log("Got row 0 (too short), got", row)
+		t.Fail()
+	}
+
+	seat = "FBFBBFFF"
+	row = findSeatRow(seat)
+	if row != 0 {
+		t.Log("Got row 0 (too long), got", row)
+		t.Fail()
+	}
+
+	seat = "9FBFBBF"
+	row = findSeatRow(seat)
+	if row != 0 {
+		t.Log("Got row 0 (non-F|B character), got", row)
+		t.Fail()
+	}
+}
+
+func TestFindSeatColumn(t *testing.T) {
+	column := findSeatColumn("RLR")
+	if column != 5 {
+		t.Log("Expected column 5, got", column)
+		t.Fail()
+	}
+
+	column = findSeatColumn("RR")
+	if column != 0 {
+		t.Log("Got column 0 (too short), got", column)
+		t.Fail()
+	}
+
+	column = findSeatColumn("RLRL")
+	if column != 0 {
+		t.Log("Got column 0 (too long), got", column)
+		t.Fail()
+	}
+
+	column = findSeatColumn("9RL")
+	if column != 0 {
+		t.Log("Got column 0 (non-L|R character), got", column)
+		t.Fail()
+	}
+}
+
+func TestGenerateSeatID(t *testing.T) {
+	seatID := generateSeatID(44, 5)
+	if seatID != 357 {
+		t.Log("Wrong seat ID, expected 357, got", seatID)
+		t.Fail()
+	}
+
+	// Top of allowable row
+	seatID = generateSeatID(127, 1)
+	if seatID != 1017 {
+		t.Log("Wrong seat ID, expected 1017, got", seatID)
+		t.Fail()
+	}
+
+	// Top of allowable column
+	seatID = generateSeatID(1, 1)
+	if seatID != 9 {
+		t.Log("Wrong seat ID, expected 9, got", seatID)
+		t.Fail()
+	}
+
+	seatID = generateSeatID(-1, 5)
+	if seatID != 0 {
+		t.Log("Wrong seat ID, expected 0 (negative row), got", seatID)
+		t.Fail()
+	}
+
+	seatID = generateSeatID(128, 1)
+	if seatID != 0 {
+		t.Log("Wrong seat ID, expected 0 (high row), got", seatID)
+		t.Fail()
+	}
+
+	seatID = generateSeatID(1, -1)
+	if seatID != 0 {
+		t.Log("Wrong seat ID, expected 0 (negative column), got", seatID)
+		t.Fail()
+	}
+
+	seatID = generateSeatID(1, 8)
+	if seatID != 0 {
+		t.Log("Wrong seat ID, expected 0 (high column), got", seatID)
+		t.Fail()
+	}
+}
+
+func TestFindHighestSeatID(t *testing.T) {
+	var seatID int
+	var err error
+	seatID, err = findHighestSeatID([]int{4, 6, 7, 2, 1, 9})
+	if seatID != 9 {
+		t.Log("Wrong seat ID, expected 9, got", seatID)
+		t.Fail()
+	}
+
+	seatID, err = findHighestSeatID([]int{})
+	if err == nil {
+		t.Log("Expected an error, for an empty seat array but didn't get one")
+		t.Fail()
+	}
+
+}
